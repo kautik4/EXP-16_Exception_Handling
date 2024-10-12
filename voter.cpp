a4a298fed35084eae6905e1e78ce6bb22987aaad
kautik <78012455+kautik4@users.noreply.github.com>
2024-10-12 11:38:22 +0530

    Add files via upload

---
+++ voter.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"enter age: ";
+    cin>>age;
+    try{
+        if(age<18){
+            throw age;
+        }
+        else{
+            cout<<"You are eligible to vote"<<endl;
+        }
+    
+    }
+    catch(int num){
+        cout<<"ERROR: required age: 18, your age: "<<num;
+        }
+}
+/*
+enter age: 17
+ERROR: required age: 18, your age:17
+*/
